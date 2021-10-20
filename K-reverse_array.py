ab2ebd34073ca284350a385c5d015c0ea9d4e654
sanchitanwar0007 <sanchitanwar0007@gmail.com>
2021-10-20 19:02:27 +0530

    k-reversed array pushed

--- K-reverse_array.py
+++ K-reverse_array.py
@@ -1,7 +1,18 @@
 n=int(input())
 arr=list(map(int,input().split()))
+end=-1
+start=-1
 for i in range(len(arr)-1,-1,-1):
-    if(arr[i]<arr[i-1]):
-        print(arr[i])
+    if(arr[i]<arr[i-1] and i!=0):
+        # print("cos")
+        end=i
         break
 
+for i in range(len(arr)-1):
+    if(arr[i]>arr[i+1]):
+        start=i
+        break
+if(end-start==0):
+    print(-1,-1,-1)
+else:
+    print(start,end,end-start+1)
